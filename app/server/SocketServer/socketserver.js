46b5a540009f307352d46667d4c123bea0e84442
Randy Mikkelsaar <randy.mikkelsaar@ek3.com>
2016-08-30 17:02:54 -0400

    Removing uneeded packages, adding pub/sub redis intro

--- app/server/SocketServer/socketserver.js
+++ app/server/SocketServer/socketserver.js
@@ -6,64 +6,59 @@ const Store = require(base`Redis`);
 const pub = require(base`Redis`);
 const sub = require(base`Redis`);
 
-var sessionMiddleware = session({
-	store: Store,
-	secret: 'redis chat'
-});
-
 var io;
 var users = {};
 module.exports = function(server) {
-	if (!server) {
-		throw new Error('SocketServer::constructor() - No server object provided');
-	}
-	if (!server && io) {
-		return io;
-	}
-	// Initialize SocketIO
-	io = require('socket.io')(server);
+    if (!server && io) {
+        return io;
+    }
+    if (!server) {
+        throw new Error('SocketServer::constructor() - No server object provided');
+    }
 
-	// Set redis store middleware
-	io.use((socket, next) => {
-		sessionMiddleware(socket.request, socket.request.res, next);
-	});
-	io.use(sessionMiddleware);
+    // Initialize SocketIO
+    io = require('socket.io')(server);
 
-	io.on('connection', function(socket) {
-		console.log('New connection');
-		socket.on('disconnect', onDisconnection.bind(socket));
-		socket.on('user login', onUserLogin.bind(socket));
-		socket.on('chat message', onChatMessage.bind(socket));
-	});
-	return io;
+    io.on('connection', function(socket) {
+        console.log('New connection');
+        socket.on('disconnect', onDisconnection.bind(socket));
+        socket.on('user login', onUserLogin.bind(socket));
+        socket.on('chat message', onChatMessage.bind(socket));
+    });
+    return io;
 };
 
+sub.on('message', onRedisMessage.bind(this));
+sub.subscribe('chat::message');
 
+function onRedisMessage(channel, message) => {
+	io.emit('chat message', message);
+}
 
 function onDisconnection() {
-	console.log(`${this.username} disconnected`);
+    console.log(`${this.username} disconnected`);
 }
 
 function onUserLogin(username) {
-	if (!username) {
-		console.warn('Username not provided');
-		return;
-	}
-	this.username = username;
-	users[this.username] = this;
-	console.log(`${this.username} has logged into the server.`);
-	this.broadcast.emit('user login', this.username);
+    if (!username) {
+        console.warn('Username not provided');
+        return;
+    }
+    this.username = username;
+    users[this.username] = this;
+    console.log(`${this.username} has logged into the server.`);
+    this.broadcast.emit('user login', this.username);
 }
 
 function onChatMessage(message) {
-	if (!this.username) {
-		console.log('User has no username');
-		return;
-	}
-	var msgObj = {
-		user: this.username,
-		payload: message
-	};
-	io.emit('chat message', msgObj);
-	console.log(`${this.username}: ${msgObj.payload}`);
+    if (!this.username) {
+        console.log('User has no username');
+        return;
+    }
+    var msgObj = {
+        user: this.username,
+        payload: message
+    };
+    pub.publish('chat::message', msgObj);
+    console.log(`Socket::onChatMessage() - ${this.username}: ${msgObj.payload}`);
 }
